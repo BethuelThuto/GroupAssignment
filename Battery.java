93f668f4f68e4f78f1c3b4f06b7a53879ac2ab9b
ResegoMogotsi <resegomogotsi9@gmail.com>
2025-04-16 22:07:32 +0200

    Add files via upload

---
+++ Battery.java
@@ -0,0 +1,15 @@
+public class Battery {
+    private String brand;
+
+    public Battery(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand; }
+    
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
